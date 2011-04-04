0d0e918c0075354f9c6e0338b19f638faea57c61
Dustin Diaz <polvero@gmail.com>
2011-04-04 14:59:50 -0700

    add new files

---
+++ make/build.js
@@ -0,0 +1,25 @@
+require('../build/smoosh').config({
+  "JAVASCRIPT": {
+    "DIST_DIR": "./",
+    "ender": [
+      "./src/copyright.js",
+      "./build/klass/src/klass.js",
+      "./build/qwery/src/qwery.js",
+      "./build/reqwest/src/reqwest.js",
+      "./build/script/src/script.js",
+      "./src/ender.js"
+    ]
+  },
+  "JSHINT_OPTS": {
+    "boss": true,
+    "forin": true,
+    "curly": true,
+    "debug": false,
+    "devel": false,
+    "evil": false,
+    "regexp": false,
+    "undef": false,
+    "sub": false,
+    "asi": false
+  }
+}).run().build().analyze();
---
+++ src/copyright.js
@@ -0,0 +1,5 @@
+/*!
+  * Ender.js - the faster, smaller JavaScript library
+  * License MIT
+  * copyright Dustin Diaz 2011
+  */
