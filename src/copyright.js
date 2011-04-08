5ce2858bebca1d3e77f4ac2646be901409068845
Dustin Diaz <polvero@gmail.com>
2011-04-08 01:30:11 -0700

    add jacob to copyright

--- src/copyright.js
+++ src/copyright.js
@@ -1,6 +1,6 @@
 /*!
-  * Ender.js: a small yet powerful JavaScript library composed of application agnostic submodules
-  * License MIT
-  * copyright Dustin Diaz 2011
+  * Ender.js: a small, powerful JavaScript library composed of application agnostic submodules
+  * copyright Dustin Diaz & Jacob Thornton 2011 (@ded @fat)
   * https://github.com/ded/Ender.js
+  * License MIT
   */
