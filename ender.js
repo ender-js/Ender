e27e511dfe5beb66a60624205f0f2f532534d022
Jacob Thornton <jacobthornton@gmail.com>
2011-04-12 11:01:33 -0700

    update ender with latest bean (fix for ender bridge)

--- ender.js
+++ ender.js
@@ -318,9 +318,10 @@ $._select = qwery.noConflict();/*!
   * the entire mootools team: github.com/mootools/mootools-core
   */
 !function (context) {
-  var _uid = 1, registry = {}, collected = {},
+  var __uid = 1, registry = {}, collected = {},
       overOut = /over|out/,
-      namespace = /.*(?=\..*)\.|.*/,
+      namespace = /[^\.]*(?=\..*)\.|.*/,
+      stripName = /\..*/,
       addEvent = 'addEventListener',
       attachEvent = 'attachEvent',
       removeEvent = 'removeEventListener',
@@ -342,7 +343,7 @@ $._select = qwery.noConflict();/*!
   }
 
   function retrieveUid(obj, uid) {
-    return (obj._uid = uid || obj._uid || _uid++);
+    return (obj.__uid = uid || obj.__uid || __uid++);
   }
 
   function listener(element, type, fn, add, custom) {
@@ -370,7 +371,7 @@ $._select = qwery.noConflict();/*!
   }
 
   function addListener(element, orgType, fn, args) {
-    var type = orgType.replace(/\..*/, ''), events = retrieveEvents(element),
+    var type = orgType.replace(stripName, ''), events = retrieveEvents(element),
         handlers = events[type] || (events[type] = {}),
         uid = retrieveUid(fn, orgType.replace(namespace, ''));
     if (handlers[uid]) {
@@ -389,20 +390,25 @@ $._select = qwery.noConflict();/*!
     }
     listener(element, isNative ? type : 'propertychange', fn, true, !isNative && true);
     handlers[uid] = fn;
-    fn._uid = uid;
+    fn.__uid = uid;
     return type == 'unload' ? element : (collected[retrieveUid(element)] = element);
   }
 
-  function removeListener(element, type, handler) {
-    var events = retrieveEvents(element);
+  function removeListener(element, orgType, handler) {
+    var uid, names, uids, i, events = retrieveEvents(element), type = orgType.replace(stripName, '');
     if (!events || !events[type]) {
       return element;
     }
-    handler = events[type][handler._uid];
-    delete events[type][handler._uid];
-    type = customEvents[type] ? customEvents[type].base : type;
-    var isNative = element[addEvent] || nativeEvents.indexOf(type) > -1;
-    listener(element, isNative ? type : 'propertychange', handler, false, !isNative && type);
+    names = orgType.replace(namespace, '');
+    uids = names ? names.split('.') : [handler.__uid];
+    for (i = uids.length; i--;) {
+      uid = uids[i];
+      handler = events[type][uid];
+      delete events[type][uid];
+      type = customEvents[type] ? customEvents[type].base : type;
+      var isNative = element[addEvent] || nativeEvents.indexOf(type) > -1;
+      listener(element, isNative ? type : 'propertychange', handler, false, !isNative && type);
+    }
     return element;
   }
 
@@ -434,19 +440,26 @@ $._select = qwery.noConflict();/*!
     return element;
   }
 
-  function remove(element, events, fn) {
-    var k, type, isString = typeof(events) == 'string', rm = removeListener, attached = retrieveEvents(element);
-    if (isString && /\s/.test(events)) {
-      events = events.split(' ');
-      var i = events.length - 1;
-      while (remove(element, events[i]) && i--) {}
+  function remove(element, orgEvents, fn) {
+    var k, type, events,
+        isString = typeof(orgEvents) == 'string',
+        names = isString && orgEvents.replace(namespace, ''),
+        rm = removeListener,
+        attached = retrieveEvents(element);
+    if (isString && /\s/.test(orgEvents)) {
+      orgEvents = orgEvents.split(' ');
+      var i = orgEvents.length - 1;
+      while (remove(element, orgEvents[i]) && i--) {}
       return element;
     }
+    events = isString ? orgEvents.replace(stripName, '') : orgEvents;
     if (!attached || (isString && !attached[events])) {
       return element;
     }
     if (typeof fn == 'function') {
       rm(element, events, fn);
+    } else if (names) {
+      rm(element, orgEvents);
     } else {
       rm = events ? rm : remove;
       type = isString && events;
@@ -461,12 +474,15 @@ $._select = qwery.noConflict();/*!
   function fire(element, type) {
     var evt, k, i, types = type.split(' ');
     for (i = types.length; i--;) {
-      type = types[i].replace(/\..*/, '');
+      type = types[i].replace(stripName, '');
       var isNative = nativeEvents.indexOf(type) > -1,
-          namespace = types[i].replace(namespace, ''),
+          isNamespace = types[i].replace(namespace, ''),
           handlers = retrieveEvents(element)[type];
-      if (namespace) {
-        handlers[namespace] && handlers[namespace]();
+      if (isNamespace) {
+        isNamespace = isNamespace.split('.');
+        for (k = isNamespace.length; k--;) {
+          handlers[isNamespace[k]] && handlers[isNamespace[k]]();
+        }
       } else if (element[addEvent]) {
         evt = document.createEvent(isNative ? "HTMLEvents" : "UIEvents");
         evt[isNative ? 'initEvent' : 'initUIEvent'](type, true, true, context, 1);
@@ -550,7 +566,7 @@ $._select = qwery.noConflict();/*!
   var bean = { add: add, remove: remove, clone: clone, fire: fire };
 
   var clean = function (el) {
-    var uid = remove(el)._uid;
+    var uid = remove(el).__uid;
     if (uid) {
       delete collected[uid];
       delete registry[uid];
@@ -579,14 +595,14 @@ $._select = qwery.noConflict();/*!
 }(this);!function () {
   var b = bean.noConflict(),
       integrate = function (method, type, method2) {
-        var args = type ? [type] : [];
+        var _args = type ? [type] : [];
         return function () {
           for (var i = 0, l = this.elements.length; i < l; i++) {
-            args.unshift(this.elements[i]);
+            var args = [this.elements[i]].concat(_args);
             b[method].apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
           }
           return this;
-        }
+        };
       };
 
   var add = integrate('add'),
