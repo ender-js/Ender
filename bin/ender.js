ee6b891e19665ea8051ad4f73699cd930f0eb2f1
Jacob Thornton <jacobthornton@gmail.com>
2011-04-15 23:28:47 -0700

    complete rewrite of ender builder  + uses bundle install if npm version < 1 else uses regular npm install (for rc 1 stuff)  + removes all depencies except color + uglify js (for faster loading when doing bundling)  + no longer goes to github for any dependencies (this means you don't have to be connected to the internet to build (assuming you already have NPM installed your desired packages  + adds new cli api:
       build (or -b): regular build
       async (or -a): creates an async build, which uses script.js to asyncronously load modules from node_modules
       help: explains api
    
     + cleans up api (much less code, less dependcies loaded into node_modules dir

---
+++ bin/ender.js
@@ -0,0 +1,166 @@
+/*!
+  * Ender.js: a small, powerful JavaScript library composed of application agnostic submodules
+  * copyright Dustin Diaz & Jacob Thornton 2011 (@ded @fat)
+  * https://github.com/ded/Ender.js
+  * License MIT
+  */
+!function (context) {
+
+  function aug(o, o2) {
+    for (var k in o2) {
+      o[k] = o2[k];
+    }
+  }
+
+  function _$(s, r) {
+    this.elements = $._select(s, r);
+    this.length = this.elements.length;
+    for (var i = 0; i < this.length; i++) {
+      this[i] = this.elements[i];
+    }
+  }
+
+  function $(s, r) {
+    return new _$(s, r);
+  }
+
+  aug($, {
+    ender: function (o, proto) {
+      aug(proto ? _$.prototype : $, o);
+    },
+    _select: function () {
+      return [];
+    }
+  });
+
+  var old = context.$;
+  $.noConflict = function () {
+    context.$ = old;
+    return this;
+  };
+
+  (typeof module !== 'undefined') && module.exports ?
+    (module.exports = $) :
+    (context.$ = $);
+
+}(this);
+
+/*!
+  * $script.js v1.3
+  * https://github.com/ded/script.js
+  * Copyright: @ded & @fat - Dustin Diaz, Jacob Thornton 2011
+  * Follow our software http://twitter.com/dedfat
+  * License: MIT
+  */
+!function(win, doc, timeout) {
+  var script = doc.getElementsByTagName("script")[0],
+      list = {}, ids = {}, delay = {}, re = /^i|c/,
+      scripts = {}, s = 'string', f = false, i,
+      push = 'push', domContentLoaded = 'DOMContentLoaded', readyState = 'readyState',
+      addEventListener = 'addEventListener', onreadystatechange = 'onreadystatechange',
+      every = function(ar, fn) {
+        for (i = 0, j = ar.length; i < j; ++i) {
+          if (!fn(ar[i])) {
+            return 0;
+          }
+        }
+        return 1;
+      };
+      function each(ar, fn) {
+        every(ar, function(el) {
+          return !fn(el);
+        });
+      }
+
+  if (!doc[readyState] && doc[addEventListener]) {
+    doc[addEventListener](domContentLoaded, function fn() {
+      doc.removeEventListener(domContentLoaded, fn, f);
+      doc[readyState] = "complete";
+    }, f);
+    doc[readyState] = "loading";
+  }
+
+  var $script = function(paths, idOrDone, optDone) {
+    paths = paths[push] ? paths : [paths];
+    var idOrDoneIsDone = idOrDone && idOrDone.call,
+        done = idOrDoneIsDone ? idOrDone : optDone,
+        id = idOrDoneIsDone ? paths.join('') : idOrDone,
+        queue = paths.length;
+        function loopFn(item) {
+          return item.call ? item() : list[item];
+        }
+        function callback() {
+          if (!--queue) {
+            list[id] = 1;
+            done && done();
+            for (var dset in delay) {
+              every(dset.split('|'), loopFn) && !each(delay[dset], loopFn) && (delay[dset] = []);
+            }
+          }
+        }
+    if (id && ids[id]) {
+      return;
+    }
+    timeout(function() {
+      each(paths, function(path) {
+        if (scripts[path]) {
+          return;
+        }
+        scripts[path] = 1;
+        id && (ids[id] = 1);
+        var el = doc.createElement("script"),
+            loaded = 0;
+        el.onload = el[onreadystatechange] = function () {
+          if ((el[readyState] && !(!re.test(el[readyState]))) || loaded) {
+            return;
+          }
+          el.onload = el[onreadystatechange] = null;
+          loaded = 1;
+          callback();
+        };
+        el.async = 1;
+        el.src = path;
+        script.parentNode.insertBefore(el, script);
+      });
+    }, 0);
+    return $script;
+  };
+
+  $script.ready = function(deps, ready, req) {
+    deps = deps[push] ? deps : [deps];
+    var missing = [];
+    !each(deps, function(dep) {
+      list[dep] || missing[push](dep);
+    }) && every(deps, function(dep) {
+      return list[dep];
+    }) ? ready() : !function(key) {
+      delay[key] = delay[key] || [];
+      delay[key][push](ready);
+      req && req(missing);
+    }(deps.join('|'));
+    return $script;
+  };
+
+  var old = win.$script;
+  $script.noConflict = function () {
+    win.$script = old;
+    return this;
+  };
+
+  (typeof module !== 'undefined' && module.exports) ?
+    (module.exports = $script) :
+    (win.$script = $script);
+
+}(this, document, setTimeout);!function () {
+  var s = $script.noConflict();
+  $.ender({
+    script: s,
+    require: s,
+    getScript: s
+  });
+}();
+
+$.script(["node_modules/qwery/qwery.js"], "qwery");
+$.script.ready("qwery", function () {
+$._select = qwery.noConflict();
+});
