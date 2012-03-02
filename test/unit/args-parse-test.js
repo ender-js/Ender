d1da1f05d62332071f530b8238f93dd54a83a256
Rod Vagg <rod@vagg.org>
2012-03-02 22:36:30 +1100

    turn args back into 'context' string

--- test/unit/args-parse-test.js
+++ test/unit/args-parse-test.js
@@ -6,121 +6,191 @@ var buster = require('buster')
     }
 
 buster.testCase('Args parser', {
+    'parse': {
+        'test parse() exists': function () {
+          assert.isFunction(argsParse.parse)
+        }
 
-    'test parse() exists': function () {
-      assert.isFunction(argsParse.parse)
+      , 'test parse finds main command': function () {
+          var actual = argsParse.parse(buildargs('help'))
+          assert.isString(actual.main)
+          assert.equals(actual.main, 'help')
+        }
+
+      , 'test parse finds main command with trailling cruft': function () {
+          var actual = argsParse.parse(buildargs('build --debug --noop stuff here'))
+          assert.isString(actual.main)
+          assert.equals(actual.main, 'build')
+        }
+
+      , 'test parse finds main command with leading and trailling cruft': function () {
+          var actual = argsParse.parse(buildargs('--debug info --sandbox --noop --output stuff here'))
+          assert.isString(actual.main)
+          assert.equals(actual.main, 'info')
+        }
+
+      , 'test parse throws exception on no arguments': function () {
+          assert.exception(function () {
+            argsParse.parse(buildargs(''))
+          }, 'UnknownMainError')
+        }
+
+      , 'test parse throws exception on only dashed (--) arguments arguments': function () {
+          assert.exception(function () {
+            argsParse.parse(buildargs('--debug --noop'))
+          }, 'UnknownMainError')
+        }
+
+      , 'test parse throws exception on unknown build commands': function () {
+          assert.exception(function () {
+            argsParse.parse(buildargs('unknown'))
+          }, 'UnknownMainError')
+
+          assert.exception(function () {
+            argsParse.parse(buildargs('--output bar --noop'))
+          }, 'UnknownMainError')
+        }
+
+      , 'test parse returns packages non dashed arguments': function () {
+          var actual = argsParse.parse(buildargs('search --output foo bar woo hoo'))
+          assert.isArray(actual.packages)
+          assert.equals(actual.packages, [ 'bar', 'woo', 'hoo' ])
+        }
+
+      , 'test parse returns packages as empty array if none provided': function () {
+          var actual = argsParse.parse(buildargs('search'))
+          assert.isArray(actual.packages)
+          assert.equals(actual.packages.length, 0)
+        }
+
+      , 'test parse returns packages as empty array if only dashed (--) provided': function () {
+          var actual = argsParse.parse(buildargs('search --noop'))
+          assert.isArray(actual.packages)
+          assert.equals(actual.packages.length, 0)
+        }
+
+      , 'test parse returns expected object (no specials)': function () {
+          var actual = argsParse.parse(buildargs('build fee fie foe fum'))
+          assert.equals(
+              actual
+            , {
+                  main: 'build'
+                , packages: [ 'fee', 'fie', 'foe', 'fum' ]
+              }
+          )
+        }
+
+      , 'test parse returns expected object (-- long form)': function () {
+          var actual = argsParse.parse(buildargs('build fee fie foe fum --output foobar --use yeehaw --max 10 --sandbox foo bar --noop --silent --help --sans --debug'))
+          assert.equals(
+              actual
+            , {
+                  main: 'build'
+                , packages: [ 'fee', 'fie', 'foe', 'fum' ]
+                , output: 'foobar'
+                , use: 'yeehaw'
+                , max: 10
+                , sandbox: [ 'foo', 'bar' ]
+                , noop: true
+                , silent: true
+                , help: true
+                , sans: true
+                , debug: true
+              }
+          )
+        }
+
+      , 'test parse returns expected object (- short form)': function () {
+          var actual = argsParse.parse(buildargs('build fee fie foe fum -o foobar -u yeehaw -x -s -h'))
+          assert.equals(
+              actual
+            , {
+                  main: 'build'
+                , packages: [ 'fee', 'fie', 'foe', 'fum' ]
+                , output: 'foobar'
+                , use: 'yeehaw'
+                , noop: true
+                , silent: true
+                , help: true
+              }
+          )
+        }
+
+      , 'test parse returns expected object (array arg stops at next -/--)': function () {
+          var actual = argsParse.parse(buildargs('build fee fie --sandbox foo bar --noop foe fum'))
+          assert.equals(
+              actual
+            , {
+                  main: 'build'
+                , packages: [ 'fee', 'fie', 'foe', 'fum' ]
+                , sandbox: [ 'foo', 'bar' ]
+                , noop: true
+              }
+          )
+        }
+
+      , 'test parse can handle compact args': function () {
+          // normally parse knows to split off the first 2 args, "node script.js"
+          // but we want it to be able to handle arrays without it
+          var actual = argsParse.parseClean('build fee fie foe fum -o foobar -u yeehaw -x -s -h'.split(' '))
+          assert.equals(
+              actual
+            , {
+                  main: 'build'
+                , packages: [ 'fee', 'fie', 'foe', 'fum' ]
+                , output: 'foobar'
+                , use: 'yeehaw'
+                , noop: true
+                , silent: true
+                , help: true
+              }
+          )
+        }
+
     }
 
-  , 'test parse finds main command': function () {
-      var actual = argsParse.parse(buildargs('help'))
-      assert.isString(actual.main)
-      assert.equals(actual.main, 'help')
-    }
+  , 'toContextString': {
+        'test no specials': function () {
+          var actual = argsParse.toContextString(argsParse.parse(buildargs('build fee fie foe fum')))
+          assert.equals(actual, 'build fee fie foe fum')
+        }
 
-  , 'test parse finds main command with trailling cruft': function () {
-      var actual = argsParse.parse(buildargs('build --debug --noop stuff here'))
-      assert.isString(actual.main)
-      assert.equals(actual.main, 'build')
-    }
+      , 'test "-" short form': function () {
+          var ctx = argsParse.toContextString(argsParse.parse(buildargs('build fee fie foe fum -o foobar -u yeehaw -x -s -h')))
+          assert(ctx)
+          assert.equals(ctx.split(' ').length, 12)
+          ctx += ' ' // for convenience so we can match spaces around each element, even at the end
+          assert.match(ctx, /^build fee fie foe fum /)
+          assert.match(ctx, / --output foobar /)
+          assert.match(ctx, / --use yeehaw /)
+          assert.match(ctx, / --noop /)
+          assert.match(ctx, / --silent /)
+          assert.match(ctx, / --help /)
+        }
 
-  , 'test parse finds main command with leading and trailling cruft': function () {
-      var actual = argsParse.parse(buildargs('--debug info --sandbox --noop --output stuff here'))
-      assert.isString(actual.main)
-      assert.equals(actual.main, 'info')
-    }
+      , 'test array arg stops at next "-/--"': function () {
+          // this test doesn't really need to be here but we may as well confirm
+          var ctx = argsParse.toContextString(argsParse.parse(buildargs('build fee fie --sandbox foo bar --noop foe fum')))
+          assert.equals(ctx, 'build fee fie foe fum --sandbox foo bar --noop')
+        }
 
-  , 'test parse throws exception on no arguments': function () {
-      assert.exception(function () {
-        argsParse.parse(buildargs(''))
-      }, 'UnknownMainError')
-    }
-  , 'test parse throws exception on only dashed (--) arguments arguments': function () {
-      assert.exception(function () {
-        argsParse.parse(buildargs('--debug --noop'))
-      }, 'UnknownMainError')
-    }
-
-  , 'test parse throws exception on unknown build commands': function () {
-      assert.exception(function () {
-        argsParse.parse(buildargs('unknown'))
-      }, 'UnknownMainError')
-
-      assert.exception(function () {
-        argsParse.parse(buildargs('--output bar --noop'))
-      }, 'UnknownMainError')
-    }
-
-  , 'test parse returns packages non dashed arguments': function () {
-      var actual = argsParse.parse(buildargs('search --output foo bar woo hoo'))
-      assert.isArray(actual.packages)
-      assert.equals(actual.packages, [ 'bar', 'woo', 'hoo' ])
-    }
-
-  , 'test parse returns packages as empty array if none provided': function () {
-      var actual = argsParse.parse(buildargs('search'))
-      assert.isArray(actual.packages)
-      assert.equals(actual.packages.length, 0)
-    }
-
-  , 'test parse returns packages as empty array if only dashed (--) provided': function () {
-      var actual = argsParse.parse(buildargs('search --noop'))
-      assert.isArray(actual.packages)
-      assert.equals(actual.packages.length, 0)
-    }
-
-  , 'test parse returns expected object (no specials)': function () {
-      assert.equals(
-          argsParse.parse(buildargs('build fee fie foe fum'))
-        , {
-              main: 'build'
-            , packages: [ 'fee', 'fie', 'foe', 'fum' ]
-          }
-      )
-    }
-
-  , 'test parse returns expected object (-- long form)': function () {
-      assert.equals(
-          argsParse.parse(buildargs('build fee fie foe fum --output foobar --use yeehaw --max 10 --sandbox foo bar --noop --silent --help --sans --debug'))
-        , {
-              main: 'build'
-            , packages: [ 'fee', 'fie', 'foe', 'fum' ]
-            , output: 'foobar'
-            , use: 'yeehaw'
-            , max: 10
-            , sandbox: [ 'foo', 'bar' ]
-            , noop: true
-            , silent: true
-            , help: true
-            , sans: true
-            , debug: true
-          }
-      )
-    }
-
-  , 'test parse returns expected object (- short form)': function () {
-      assert.equals(
-          argsParse.parse(buildargs('build fee fie foe fum -o foobar -u yeehaw -x -s -h'))
-        , {
-              main: 'build'
-            , packages: [ 'fee', 'fie', 'foe', 'fum' ]
-            , output: 'foobar'
-            , use: 'yeehaw'
-            , noop: true
-            , silent: true
-            , help: true
-          }
-      )
-    }
-
-  , 'test parse returns expected object (array arg stops at next -/--)': function () {
-      assert.equals(
-          argsParse.parse(buildargs('build fee fie --sandbox foo bar --noop foe fum'))
-        , {
-              main: 'build'
-            , packages: [ 'fee', 'fie', 'foe', 'fum' ]
-            , sandbox: [ 'foo', 'bar' ]
-            , noop: true
-          }
-      )
+      , 'test "--" long form': function () {
+          var ctx = argsParse.toContextString(
+                argsParse.parse(
+                  buildargs('build fee fie foe fum --output foobar --use yeehaw --max 10 --sandbox foo bar --noop --silent --help --sans --debug')))
+          assert(ctx)
+          assert.equals(ctx.split(' ').length, 19)
+          ctx += ' ' // for convenience so we can match spaces around each element, even at the end
+          assert.match(ctx, /^build fee fie foe fum /)
+          assert.match(ctx, / --output foobar /)
+          assert.match(ctx, / --use yeehaw /)
+          assert.match(ctx, / --max 10 /)
+          assert.match(ctx, / --sandbox foo bar /)
+          assert.match(ctx, / --noop /)
+          assert.match(ctx, / --silent /)
+          assert.match(ctx, / --help /)
+          assert.match(ctx, / --sans /)
+          assert.match(ctx, / --debug /)
+        }
     }
 })
