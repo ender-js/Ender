fc1b2f593b2b084238d555b970eb96d6ecf6b9f3
Rod Vagg <rod@vagg.org>
2012-03-31 20:00:30 +1100

    removed integration tests (for now)

--- buster.js
+++ buster.js
@@ -31,12 +31,6 @@ config['unit'] = {
   , libs: [ 'test/common.js' ]
 }
 
-config['integration'] = {
-    environment: 'node'
-  , tests: [ 'test/integration/*-test.js' ]
-  , libs: [ 'test/common.js' ]
-}
-
 config['functional'] = {
     environment: 'node'
   , tests: [ 'test/functional/*-test.js' ]
--- test/integration/tmp-test.js
+++
@@ -1,8 +0,0 @@
-var buster = require('buster')
-  , assert = buster.assert
-
-buster.testCase('integration', {
-  'nothing to see here (yet)': function () {
-    assert(true)
-  }
-})
