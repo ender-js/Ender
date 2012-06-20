288bbf81f92c0dfcdb20caf1320a50993baddd48
Rod Vagg <rod@vagg.org>
2012-06-20 10:11:41 +1000

    npm@1.1.30 with `logstream`! to replace `logfd`

--- test/functional/common.js
+++ test/functional/common.js
@@ -127,8 +127,6 @@ var mktmpdir = function (callback) {
 
   , enderpath = path.resolve(__dirname, '../../bin/ender')
 
-  , __tmpNpmfixNote = false
-
   , runEnder = function (cmd, files, dir, callback) {
       if (Array.isArray(files)) files = { expectedFiles: files };
       var run = function (dir) {
@@ -140,16 +138,11 @@ var mktmpdir = function (callback) {
               , stdout = ''
               , stderr = ''
 
-            if (!__tmpNpmfixNote) {
-              __tmpNpmfixNote = true
-              require('colors')
-              console.log('\nWARNING: stderr is intentionally blanked due to logfd removal from npm, waiting for fix'.magenta.bold.inverse)
-            }
             child.stdout.on('data', function (data) {
               stdout += data.toString('utf-8')
             })
             child.stderr.on('data', function (data) {
-              //FIXME: commented out while we sort out the npm logstream stuff stderr += data.toString('utf-8')
+              stderr += data.toString('utf-8')
             })
 
             child.on('exit', function (code, signal) {
